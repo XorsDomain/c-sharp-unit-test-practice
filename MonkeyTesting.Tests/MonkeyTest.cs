845bcaf1306d2dde9899cc711afa8d3587d8af98
Kyle Pike <kpike@catalyte.io>
2021-09-24 12:09:18 -0400

    formatting

--- MonkeyTesting.Tests/MonkeyTest.cs
+++ MonkeyTesting.Tests/MonkeyTest.cs
@@ -1,7 +1,6 @@
 namespace MonkeyTesting.Tests
 {
     public class MonkeyTest
-    {
-        
+    {        
     }
 }
--- MonkeyTesting/MonkeyTrouble.cs
+++ MonkeyTesting/MonkeyTrouble.cs
@@ -4,8 +4,8 @@ namespace MonkeyTesting
 {
     public class MonkeyTrouble
     {
-        public bool Trouble(bool aSmile, bool bSmile) {
-
+        public bool Trouble(bool aSmile, bool bSmile) 
+        {
         }
     }
 }
