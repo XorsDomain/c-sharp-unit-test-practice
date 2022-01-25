3cba1729b5bd4a36c5d45385472e07de9ef4bfc0
George Marshall <gmarshall@catalyte.io>
2022-01-25 11:25:29 -0500

    Created unit tests and a method to test.

--- MonkeyTesting.Tests/MonkeyTest.cs
+++ MonkeyTesting.Tests/MonkeyTest.cs
@@ -1,6 +1,56 @@
+using System;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+using MonkeyTesting;
 namespace MonkeyTesting.Tests
 {
-    public class MonkeyTest
-    {        
+    public class MonkeyTesting
+    {
+        MonkeyTrouble monkeyTrouble;
+        public MonkeyTesting()
+        {
+            monkeyTrouble = new MonkeyTrouble();
+        }
+
+        [Fact]
+        public void BothMonkeys_Smiling_ReturnTrue()
+        {
+            bool monkeyASmiling = true;
+            bool monkeyBSmiling = true;
+            var expected = true;
+            var actual = monkeyTrouble.Trouble(monkeyASmiling, monkeyBSmiling);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void OnlyMonkeyA_Smiling_ReturnsFalse()
+        {
+            bool monkeyASmiling = true;
+            bool monkeyBSmiling = false;
+            var expected = false;
+            var actual = monkeyTrouble.Trouble(monkeyASmiling, monkeyBSmiling);
+            Assert.Equal(expected, actual);
+        }
+
+                [Fact]
+        public void OnlyMonkeyB_Smiling_ReturnsFalse()
+        {
+            bool monkeyASmiling = false;
+            bool monkeyBSmiling = true;
+            var expected = false;
+            var actual = monkeyTrouble.Trouble(monkeyASmiling, monkeyBSmiling);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NeitherMonkey_Smiling_ReturnsTrue()
+        {
+            bool monkeyASmiling = false;
+            bool monkeyBSmiling = false;
+            var expected = true;
+            var actual = monkeyTrouble.Trouble(monkeyASmiling, monkeyBSmiling);
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- MonkeyTesting/MonkeyTrouble.cs
+++ MonkeyTesting/MonkeyTrouble.cs
@@ -4,8 +4,17 @@ namespace MonkeyTesting
 {
     public class MonkeyTrouble
     {
-        public bool Trouble(bool aSmile, bool bSmile) 
+        public bool Trouble(bool aSmile, bool bSmile)
         {
+            if (aSmile != bSmile)
+            {
+                return false;
+            }
+            // else 
+            // {
+            //     return true;
+            // }
+            return true;
         }
     }
 }
